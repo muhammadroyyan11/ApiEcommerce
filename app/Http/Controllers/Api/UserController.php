36429557fa8b4b6fa1551c7e82f40791dac8889a
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2023-08-31 09:11:02 +0700

    Halo

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Transaksi;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
@@ -54,6 +55,85 @@ class UserController extends Controller
                 'success' => 1,
                 'message' => 'Selamat datang Register Berhasil',
                 'user' => $user
+            ]);
+        }
+
+        return $this->error('Registrasi gagal');
+    }
+
+    public function update_profile(Request $request){
+        $user = User::all()->where('email', $request->old_email)->first();
+
+        $validasi = Validator::make($request->all(), [
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required|unique:users'
+        ]);
+
+        if ($validasi->fails()) {
+            $val = $validasi->errors()->all();
+            return $this->error($val[0]);
+        }
+
+        if($user){
+            if ($user->phone == $request->phone){
+                $user->update([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                ]);
+            } else {
+                $user->update([
+                    'name' => $request->name,
+                    'phone' => $request->phone,
+                    'email' => $request->email,
+                ]);
+            }
+
+            return response()->json([
+                'success' => 1,
+                'message' => 'Update profile berhasil',
+                'user' => $user
+            ]);
+        }
+        return $this->error('Update profile gagal');
+    }
+    public function delete(Request $request){
+        $user = User::all()->where('email', $request->email)->first();
+
+        if($user){
+            $user->delete();
+
+            return response()->json([
+                'success' => 1,
+                'message' => 'Delete profile berhasil',
+                'user' => $user
+            ]);
+        }
+        return $this->error('Delete profile gagal');
+    }
+
+    public function update_password(Request $requset){
+        //nama, email, password
+        $user = User::all()->where('email', $requset->email)->first();
+
+        $validasi = Validator::make($requset->all(), [
+            'password' => 'required|min:6'
+        ]);
+
+        if($validasi->fails()){
+            $val = $validasi->errors()->all();
+            return $this->error($val[0]);
+        }
+
+        if($user){
+            $user->update([
+                'password' => bcrypt($requset->password)
+            ]);
+
+            return response()->json([
+                'success' => 1,
+                'message' => 'Update password berhasil',
+                'user' => $requset->password
             ]);
         }
 
